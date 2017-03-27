745d9e77be1ee4554b96abe94afe7cfbb5553349
Wilder <wildermachadodacruz@gmail.com>
2017-03-27 15:33:56 -0300

    Controller vazios

---
+++ app/Http/Controllers/DisciplinaController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace eeducar\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DisciplinaController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/SemestreController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace eeducar\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SemestreController extends Controller
+{
+    //
+}
