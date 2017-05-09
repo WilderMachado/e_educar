fad20c9cd7a44859c678e9194737aee644ccf3a1
Wilder <wildermachadodacruz@gmail.com>
2017-05-09 11:58:51 -0300

    Inclusão de javascript de aluno e comentários em AlunoController

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -9,7 +9,6 @@ use eeducar\Turma;
 use eeducar\User;
 use Illuminate\Support\Facades\Input;
 
-
 class AlunoController extends Controller
 {
     /**
@@ -23,92 +22,113 @@ class AlunoController extends Controller
         return view('aluno.index', compact('alunos'));  //Redireciona para a view com os alunos
     }
 
+    /**Método para chamar tela para cadastro de novo aluno
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function novo()
     {
-        $turmas = Turma::orderBy('codigo')->pluck('codigo', 'id');
-        $responsaveis = User::where('role', 'responsavel')->orderBy('name')->pluck('name', 'id');
-        return view('aluno.novo', compact('turmas', 'responsaveis'));
+        $turmas = Turma::orderBy('codigo')->pluck('codigo', 'id');      //Busca turmas ordenando por código, pegando código e id
+        $responsaveis = User::where('role', 'responsavel')              //Busca usuário com papel de responsável
+        ->orderBy('name')->pluck('name', 'id');                         //Ordena por nome e pega nome e id
+        return view('aluno.novo', compact('turmas', 'responsaveis'));   //Redireciona para view de cadastro
     }
 
+    /**Método que salva novo aluno
+     * @param AlunoRequest $request dados do aluno a ser cadastrado
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
     public function salvar(AlunoRequest $request)
     {
         $this->validate($request,
-            ['matricula' => 'unique:alunos,matricula',
-                'email' => 'unique:alunos,email',
+            ['matricula' => 'unique:alunos,matricula',  //Valida matrícula
+                'email' => 'unique:alunos,email',       //Valida e-mail
             ]);
-        $aluno = new Aluno($request->all());
+        $aluno = new Aluno($request->all());            //Cria novo aluno com dados passados
+        //Salva arquivo com foto passando o arquivo, o nome da pasta e matrícula do aluno, guardando o caminho da foto
         $aluno->foto = ManipuladorArquivo::salvar($request->file('foto'), 'alunos', $aluno->matricula);
+        //Salva dados do responsável, passando aluno, id do responsável e dados do responsável
         $this->salvarResponsavel($aluno, $request->responsavel_id, $request->responsavel);
-        /*$responsavel_id = $request->responsavel_id;
-        if ($responsavel_id):
-            $aluno->responsavel()->associate($responsavel_id);
-        else:
-            $dados = $request->responsavel;
-            $aluno->responsavel()->associate(User::create([
-                'name' => $dados['nome'],
-                'email' => $dados['email'],
-                'password' => bcrypt($dados['password']),
-                'role' => 'responsavel'
-            ]));
-        endif;*/
-        $aluno->save();
-        return redirect('alunos');
+        $aluno->save();                                 //Salva aluno
+        return redirect('alunos');                      //Redireciona para index de alunos
     }
 
+    /**Método que chama tela para edição de dados de aluno
+     * @param $id int identificador do aluno
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function editar($id)
     {
-        $turmas = Turma::orderBy('codigo')->pluck('codigo', 'id');
-        //$turmas = Turma::find(Aluno::find($id))->pluck('codigo','id');
-        $responsaveis = User::where('role', 'responsavel')->orderBy('name')->pluck('name', 'id');
-        // $responsaveis = User::find(Aluno::find($id))->orderBy('name')->pluck('name','id');
-        $aluno = Aluno::find($id);
-        return view('aluno.editar', compact('aluno', 'turmas', 'responsaveis'));
+        $turmas = Turma::orderBy('codigo')->pluck('codigo', 'id');                  //Busca turmas ordenando pelo código
+        $responsaveis = User::where('role', 'responsavel')                          //Busca usuários com papel de responsável
+        ->orderBy('name')->pluck('name', 'id');                                     //Ordena por nome e pega nome e id
+        $aluno = Aluno::find($id);                                                  //Busca aluno pelo id passado
+        return view('aluno.editar', compact('aluno', 'turmas', 'responsaveis'));    //Redireciona para view de edição
     }
 
+    /** Método que altera dados de aluno
+     * @param AlunoRequest $request dados do aluno para edição
+     * @param $id int identificador do aluno a ser alterado
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
     public function alterar(AlunoRequest $request, $id)
     {
-        $aluno = Aluno::find($id);
-        $aluno->fill($request->all());
+        $aluno = Aluno::find($id);                      //Busca aluno pelo id
+        $aluno->fill($request->all());                  //Passa dados do request para o aluno
+        //Salva arquivo com foto passando o arquivo, o nome da pasta e matrícula do aluno, passando o caminho da foto para variável
         $foto = ManipuladorArquivo::salvar($request->file('foto'), 'alunos', $aluno->matricula);
-        if ($foto != null && $foto != $aluno->foto):
-            ManipuladorArquivo::excluir($aluno->foto);
-            $aluno->foto = $foto;
+        if ($foto != null && $foto != $aluno->foto):    //Verifica se variável é diferente de null e diferente do caminho em aluno
+            ManipuladorArquivo::excluir($aluno->foto);  //Exclui o arquivo anterior
+            $aluno->foto = $foto;                       //Passa o novo caminho para o campo foto em aluno
         endif;
+        //Salva dados do responsável, passando aluno, id do responsável e dados do responsável
         $this->salvarResponsavel($aluno, $request->responsavel_id, $request->responsavel);
-        $aluno->save();
-        return redirect('alunos');
+        $aluno->save();                                 //Salva aluno
+        return redirect('alunos');                      //Redireciona para index de alunos
     }
 
+    /**Método para busca de nome e e-mail de responsavel de aluno
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function buscarResponsavel()
     {
+        //Seleciona nome e email de responsável cujo id foi passado e retorna em forma de json
         return response()->json(User::select('name', 'email')->find(Input::get('responsavel_id')));
     }
 
+    /**Método de exclusão de aluno
+     * @param $id int identificador do aluno
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
     public function excluir($id)
     {
-        Aluno::find($id)->delete();
-        return redirect('alunos');
+        Aluno::find($id)->delete(); //Busca aluno pelo id e exclui
+        return redirect('alunos');  //Redireciona para index de alunos
     }
 
+    /** Método para salvar responsável por aluno
+     * @param Aluno $aluno aluno
+     * @param $responsavel_id int identificador do responsa´vel
+     * @param $dados array dados do responsável
+     */
     private function salvarResponsavel(Aluno $aluno, $responsavel_id, $dados)
     {
-        if ($responsavel_id):
-            $aluno->responsavel()->associate($responsavel_id);
-            if ($dados):
+        if ($responsavel_id):                                   //Se existir id de responsável
+            $aluno->responsavel()->associate($responsavel_id);  //Associa id do responsável ao aluno
+            if ($dados):                                        //Se existir dados
                 $aluno->responsavel()->update([
                     'name' => $dados['nome'],
                     'email' => $dados['email'],
                     'password' => bcrypt($dados['password']),
                     'role' => 'responsavel'
-                ]);
+                ]);                                             //Altera dados do responsável
             endif;
-        else:
+        else:                                                   //Se não houver id de responsável (nulo)
             $aluno->responsavel()->associate(User::create([
                 'name' => $dados['nome'],
                 'email' => $dados['email'],
                 'password' => bcrypt($dados['password']),
                 'role' => 'responsavel'
-            ]));
+            ]));                                                //Cria novo responsável e associa ao aluno
         endif;
     }
 }
---
+++ public/js/aluno.js
@@ -0,0 +1,99 @@
+/**
+ * Created by Wilder on 03/05/2017.
+ */
+window.onload = function () {                                                                   //Passa função para onload da janela
+    var responsavelId = document.getElementById("responsavel_id");                              //Pega id do responsável
+    var inputsRespnsavel = [
+        document.getElementById("responsavel[nome]"),                                           //Input de nome do responsável
+        document.getElementById("responsavel[email]"),                                          //Input de e-mail de responsável
+        document.getElementById("responsavel[password]"),                                       //Input de senha de responsável
+        document.getElementById("responsavel[password_confirmation]")                           //Input de confirmação de senha de responsável
+    ];                                                                                          //Cria array de inputs
+    if (responsavelId) {                                                                        //Se existir id de responsavel
+        var divResponsavel = document.getElementById("responsavel");                            //Pega div de responsável
+        divResponsavel.style.display = "none";                                                  //Esconde div de responsável
+        for (var i = 0; i < inputsRespnsavel.length; i++) {                                     //Itera pelos inputs de dados de responsável
+            inputsRespnsavel[i].setAttribute("disabled", "disabled");                           //Passa atributo disabled aos inputs
+        }
+        var btnNovoResponsavel = document.getElementById("btn-novo-responsavel");               //Pega botão de criar novo responsável
+        var btnEditarResponsavel = document.getElementById("btn-editar-responsavel");           //Pega botão de editar responsável
+        var btnSelecionarResponsavel = document.getElementById("btn-selecionar-responsavel");   //Pega botão de selecionar responsável
+        btnSelecionarResponsavel.style.display = "none";                                        //Esconde botão de selecionar responsável
+        btnNovoResponsavel.addEventListener("click", function () {                              //Atribui função a click de botão de criar novo responsável
+            this.style.display = "none";                                                        //Esconde o botão de criar novo responsável
+            divResponsavel.style.display = "inline";                                            //Torna visível a div de responsável
+            btnSelecionarResponsavel.style.display = "inline";                                  //Torna visível o botão de selecionar responsável
+            responsavelId.parentNode.parentNode.style.display = "none";                         //Esconde a seleção de responsável
+            responsavelId.setAttribute("disabled", "disabled");                                 //Passa atributo disabled à seleção de responsável
+            if (btnEditarResponsavel) {                                                         //Se existir botão de editar responsável
+                btnEditarResponsavel.style.display = "none";                                    //Esconde o botão de editar responsável
+            }
+            for (var i = 0; i < inputsRespnsavel.length; i++) {                                 //Itera pelos inputs de dados de responsável
+                inputsRespnsavel[i].removeAttribute("disabled");                                //Remove atributo disabled aos inputs
+                inputsRespnsavel[i].value = null;                                               //Atribui null ao valor dos inputs de responsável
+            }
+        });
+        btnSelecionarResponsavel.addEventListener("click", function () {                        //Atribui função a click de botão de selecionar responsável
+            this.style.display = "none";                                                        //Esconde o botão de seleção de responsável
+            divResponsavel.style.display = "none";                                              //Esconde a div de responsável
+            btnNovoResponsavel.style.display = "inline";                                        //Torna visível botão de criar novo responsável
+            if (btnEditarResponsavel) {                                                         //Se existir botão de editar responsável
+                btnEditarResponsavel.style.display = "inline";                                  //Torna visível botão de editar responsável
+            }
+            responsavelId.parentNode.parentNode.style.display = "inline";                       //Torna visível seleção de responsável
+            responsavelId.removeAttribute("disabled");                                          //Remove atributo disabled da seleção de responsável
+            for (var i = 0; i < inputsRespnsavel.length; i++) {                                 //Itera pelos inputs de responsável
+                inputsRespnsavel[i].setAttribute("disabled", "disabled");                       //Passa atributo disabled aos inputs de responsável
+            }
+        });
+        if (btnEditarResponsavel) {                                                             //Se existir botão de editar responsável
+            btnEditarResponsavel.addEventListener("click", function () {                        //Atribui função para click de botão de editar responsável
+                    buscarResponsavel(responsavelId.value);                                     //Executa função buscarResponsável, passando valor selecionado
+                    this.style.display = "none";                                                //Esconde botão de editar responsável
+                    btnNovoResponsavel.style.display = "inline";                                //Torna visível botão de criar novo responsável
+                    btnSelecionarResponsavel.style.display = "inline";                          //Torna visível botão de selecionar responsável
+                    for (var i = 0; i < inputsRespnsavel.length; i++) {                         //Itera pelos inputs de responsável
+                        inputsRespnsavel[i].removeAttribute("disabled");                        //Remove atributo disabled dos inputs de responsável
+                    }
+                    responsavelId.parentNode.parentNode.style.display = "none";                 //Esconde seleção de responsável
+                    divResponsavel.style.display = "inline";                                    //Torna visível div de responsável
+                }
+            );
+        }
+    }
+};
+/**
+ * Função ajax que realiza busca de dados de responsável
+ * @param id identificador do responsável
+ */
+function buscarResponsavel(id) {
+
+    var caminho = "buscarResponsavel?responsavel_id=" + id;                                     //criar variável com caminho passando id do responsável
+    var xhttp = criarXHTTP();                                                                   //Recebe objeto XMLHttpRequest por meio de função
+    xhttp.onreadystatechange = function () {                                                    //Passa função para atributo onreadystatechange do XMLHttpRequest
+        if (xhttp.readyState == 4 && xhttp.status == 200) {                                     //Se atributo readyState for 4 e status for 200 no XMLHttpRequest
+            var json = JSON.parse(xhttp.responseText);                                          //Converte em json e atribui a variável a resposta obtida
+            document.getElementById("responsavel[nome]").value = json.name;                     //Busca elemento com nome do responsável e passa o valor obtido
+            document.getElementById("responsavel[email]").value = json.email;                   //Busca elemento com email do responsável e passa o valor obtido
+        }
+    };
+    xhttp.open("GET", caminho, true);                                                           //Abre a chamada passando o método GET, o caminho e true para operação assincrona
+    xhttp.send();                                                                               //Executa requisição
+}
+/**
+ * Método para criar objeto XMLHttpRequest
+ * @returns {*} objeto XMLHttpRequest
+ */
+function criarXHTTP() {
+    var xhttp = null;                                                                           //Cria variável para objeto
+    if (window.XMLHttpRequest) {                                                                //Verifica se existe objeto XMLHttpRequest
+        xhttp = new XMLHttpRequest();                                                           //Cria novo XMLHttpRequest (Maioria dos navegadores)
+    } else if (window.ActiveXObject) {                                                          //Se não, se existe ActiveXObject (IE)
+        try {
+            xhttp = new ActiveXObject("Msxml2.XMLHTTP");                                        //Tenta criar ActiveXObject(Msxml2.XMLHTTP) IE mais recentes
+        } catch (e) {                                                                           //Se não conseguir
+            xhttp = new ActiveXObject("Microsoft.XMLHTTP");                                     //Tenta criar ActiveXObject(Microsoft.XMLHTTP) IE inferior a 5
+        }
+    }
+    return xhttp;                                                                               //Retorna objeto XMLHttpRequest
+}
